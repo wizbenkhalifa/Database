933b4f763c3fc8d6ed02587ddfbae142e9b2feaf
benkhalifayoussef <benkhalifayoussef@Lab06_01.itseinaudi.local>
2016-12-20 11:09:21 +0100

    Aggiusta Libreria

--- Database/src/Database.java
+++ Database/src/Database.java
@@ -189,35 +189,27 @@ public class Database {
 
 	}
 	
-	public static void updateDatabase(){
+	public static void updateDatabase(String Data) throws SQLException, ClassNotFoundException{
 		Connection cn;
 		Statement st;
 		String sql;
 		// ________________________________connessione
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException: ");
-			System.err.println(e.getMessage());
-		} // fine try-catch
-		try {
-			// Creo la connessione al database
-			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/carsharing?user=root&password=");
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		
+		// Creo la connessione al database
+		cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/carsharing?user=root&password=");
 
-			// Recupero l'id. Se volessi fare altri controlli potrei recuperare
-			// anche gli altri valori
+		// Recupero l'id. Se volessi fare altri controlli potrei recuperare
+		// anche gli altri valori
 
-			sql = "SELECT * FROM auto";
-			System.out.println(sql); // stampa la query
-			// ________________________________query
-
-			st = cn.createStatement(); // creo sempre uno statement sulla
-										// connessione
-			st.execute(sql); // faccio la query su uno statement
-			cn.close(); // chiusura connessione
-		} catch (SQLException e) {
-			System.out.println("errore:" + e.getMessage());
-		} // fine try-catch
+		sql = "SELECT * FROM auto";
+		st = cn.createStatement(); // creo sempre uno statement sulla
+									// connessione
+		st.execute(sql); // faccio la query su uno statement
+		cn.close(); // chiusura connessione
+		
 	}
 	
 	 
